58030e49d2b40303414cb313139ad46b287d90b9
Eugenio <lugezz@gmail.com>
2023-07-09 17:27:53 -0300

    Try RouterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,22 @@
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom';
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
 import './index.css'
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
+
